6dc66c3348bf645fb1c1f007d1c4f44c2476a1aa
sunnior <sunnior@gmail.com>
2017-10-01 13:26:13 +0800

    [+] practical3 can run.

--- Code/TorchTest/Main.cpp
+++ Code/TorchTest/Main.cpp
@@ -1,9 +1,14 @@
 #include "lua.hpp"
 
+extern "C" int luaopen_libtorch(lua_State *L);
+extern "C" int luaopen_libsys(lua_State *L);
+
 int main()
 {
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
+	luaopen_libtorch(L);
+	luaopen_libsys(L);
 
 	int r = luaL_dofile(L, "practical3/simple_example.lua");
 	if (r)
