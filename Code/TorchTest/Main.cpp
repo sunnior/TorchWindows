a4efc88876df97f84a9e59df56aa356414d78b29
sunnior <sunnior@gmail.com>
2017-10-01 11:44:04 +0800

    [+] add TorchTest

---
+++ Code/TorchTest/Main.cpp
@@ -0,0 +1,16 @@
+#include "lua.hpp"
+
+int main()
+{
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+
+	int r = luaL_dofile(L, "practical3/simple_example.lua");
+	if (r)
+	{
+		printf("%s\n", lua_tostring(L, -1));
+	}
+
+	lua_close(L);
+
+}
